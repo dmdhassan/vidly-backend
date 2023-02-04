c1281f089f63ebe3bc17da77c2bc14f8241e3f1a
Hassan Yahya <hassanyahya400@gmail.com>
2023-02-04 23:15:57 +0100

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const Joi = require('joi');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const {User} = require('../models/user');
 const mongoose = require('mongoose');
@@ -13,7 +13,8 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(400).send('Invalid email or password.');
 
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  // const validPassword = await bcrypt.compare(req.body.password, user.password);
+  const validPassword = user.password === req.body.password;
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
   const token = user.generateAuthToken();
